5bb7e2cb90a3d041ce0d67ba55f19eb290fd25d8
Tirapat <mind1990@gmail.com>
2018-11-15 15:02:12 -0800

    adds form to Sign up component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const Signup = () =>
-	<div>
-		<h1>Signup Page</h1>
-	</div>
+import * as routes from '../constants/routes';
+
+class Signup extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Sign Up</h1>
+				<form onSubmit={this.handleSubmit}>
+        <input
+        	type='text' name='username'
+        	placeholder='User name'
+          onChange={this.handleChange}
+          value=''
+          required
+        />
+        <input
+        	type='password' name='password'
+        	placeholder='Password'
+          onChange={this.handleChange}
+          value=''
+          minlength='8'
+          required
+        />
+        <input
+        	type='password' name='password'
+        	placeholder='Confirm password'
+          onChange={this.handleChange}
+          value=''
+          minlength='8'
+          required
+        />
+        <input
+        	type='email' name='email'
+        	placeholder='Email address'
+          onChange={this.handleChange}
+          value=''
+          required
+        />
+        <Link to ={routes.SIGN_IN}>Already have an account? Sign In!</Link>
+        <button>Sign Up</button>
+      </form>
+			</div>
+		);
+	}
+}
+
+
+
+
 
 export default Signup;
