8cba0e116c8fc7f459694a075e0386f15fd7c2a7
Tirapat <mind1990@gmail.com>
2018-11-18 16:12:29 -0800

    adds profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import axios from 'axios';
 // import * as routes from '../constants/routes';
 
@@ -7,16 +7,21 @@ class Profile extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<h1>Profile Page</h1>
-					<h2>First name</h2>
-					<h2>Last name</h2>
-					<h2>Email</h2>
-					<button>Edit</button>
-				</div>
-				<div>
-					<h2>Date joined</h2>
-					<h1>Your posts</h1>
+				<div className="profile-container">
+					
+					<div>
+						<h2 className="fullname">Kenny</h2>
+						<h2 className="fullname">Brocky</h2>
+					</div>
+					<h2>kennybrockforever@gmail.com</h2>
+					<button id="edit-button">Edit</button>
+					<h3 className="date-join">Date joined: 10/1/2018</h3>
+					<h1 className="posts">Your posts</h1>
+					<Link to ="" className='post-link'>post</Link>
+						<Link to ="" className='post-link'>post</Link>
+						<Link to ="" className='post-link'>post</Link>
+						<Link to ="" className='post-link'>post</Link>
+						<Link to ="" className='post-link'>post</Link>
 				</div>
 			</div>
 		);
